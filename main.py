feeac98f1bc31f06e56eeac3af172fe1e72b95f2
UsmanS2 <twixmixer42@gmail.com>
2024-12-15 12:01:11 -0600

    setting up model

--- main.py
+++ main.py
@@ -25,4 +25,35 @@ import tkinter as tk
 import tensorflow as tf
 import numpy as np
 from PIL import Image, ImageDraw
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.utils import to_categorical
+
+# load the MNIST Dataset
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+# Normalize the images to range [0, 1]
+x_train = x_train / 255.0
+y_train = y_train / 255.0
+
+# Encode the labels
+y_train = to_categorical(y_train, num_classes=10)
+y_test = to_categorical(y_test, num_classes=10)
+
+
+# Build the model
+model = Sequential([
+    Flatten(input_shape=(28,28)),    # Takes the initial image and converts the 2d, 28x28, image into 1d vector
+    Dense(128, activation='relu'),   # Hidden Layer, processes flattened vector, learns patterns in the data
+    Dense(10, activation='softmax')  # output layer, outputs distribution across the 10 classes aka digits 0-9
+])
+
+
+# Compile the model
+model.compile(
+    optimizer='adam',  # Adaptive Moment Estimation, optimization algorithm
+    loss='categorical_crossentropy',  # calculates the difference between the model's predictions and the actual labels
+    metrics=['accuracy']  # used to evaluate the model's performance
+)
+
 
